72a2e5e13a3e207496bc758ea728bc96a14e1e67
Tom Carter <tom.carter@u2i.com>
2020-09-14 19:32:15 +0200

    Please!

--- s3contents/s3_fs.py
+++ s3contents/s3_fs.py
@@ -74,7 +74,7 @@ class S3FS(GenericFS):
         client_kwargs = {
             "endpoint_url": self.endpoint_url,
             "region_name": self.region_name,
-            "verify": kwargs["verify"],
+            "verify": "false",
         }
         config_kwargs = {}
         if self.signature_version:
